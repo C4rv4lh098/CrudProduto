1788e8a81d2a3546b9c00a499066defd71919cce
C4rv4lh098 <rodrigo.carvalho.1998@gmail.com>
2021-12-01 10:53:56 -0400

    Adicionando parte do front

---
+++ frontCrud/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'', component:HomeComponent},
+  {path:'cadastro', component:CadastroComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
